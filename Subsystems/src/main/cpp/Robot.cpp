ea2c78a5e84599488dff4560a2c0bca70bcf752e
23nicsat <23nicsat@student.rockway.ca>
2020-02-06 19:01:11 -0500

    I Joe lot's of stuff(Added autonmous drive little)

--- Subsystems/src/main/cpp/Robot.cpp
+++ Subsystems/src/main/cpp/Robot.cpp
@@ -20,12 +20,12 @@ frc::XboxController m_stick {RobotMap::Controllers::XBOX};
 frc::Joystick m_stick2 {RobotMap::Controllers::FLIGHT};
 
 void Robot::RobotInit() {
-  #if defined(__linux__)
+ /* #if defined(__linux__)
     frc::CameraServer::GetInstance()->StartAutomaticCapture();
 #else
     wpi::errs() << "Vision only available on Linux.\n";
     wpi::errs().flush();
-#endif
+#endif*/
 }
 
 /**
